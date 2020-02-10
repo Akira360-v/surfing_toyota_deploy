ddc27f14c632bd914e10b3cdbd2ecbfb684ce187
Akira360 <akira360.v@gmail.com>
2020-02-10 10:02:54 +0300

    rt1

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,21 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/about1',
+    name: 'about1',
+    component: () => import(/* webpackChunkName: "about" */ '../views/test/About1.vue')
+  },
+  {
+    path: '/about2',
+    name: 'about2',
+    component: () => import(/* webpackChunkName: "about" */ '../views/test/About2.vue')
+  },
+  {
+    path: '/about3',
+    name: 'about3',
+    component: () => import(/* webpackChunkName: "about" */ '../views/test/About3.vue')
   }
 ]
 
