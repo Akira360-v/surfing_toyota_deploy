042bb58f588ba484f14d8459d200e178e64e9709
Akira360 <akira360.v@gmail.com>
2020-02-11 17:01:07 +0300

    rt07+++

---
+++ src/components/adventure/AdventureExtremes.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="adventure__extremes">
+    <div class="container">
+      <div class="extremes">
+        <div class="extremes__head">
+          ты можешь больше
+        </div>
+        <div class="extremes__text">
+          посмотри и убедись в этом
+        </div>
+        <div class="extremes__block">
+          <div class="landings">
+            <div class="landings__item">
+              <figure>
+                <img src="@/assets/img/adventure/prev/one.png" alt="1">
+              </figure>
+              <div class="landings__text">
+                СЁрфинг
+              </div>
+            </div>
+            <div class="landings__item">
+              <figure>
+                <img src="@/assets/img/adventure/prev/the.png" alt="2">
+              </figure>
+              <div class="landings__text">
+                СНОУБОРДИНГ
+              </div>
+            </div>
+            <div class="landings__item">
+              <figure>
+                <img src="@/assets/img/adventure/prev/twee.png" alt="3">
+              </figure>
+              <div class="landings__text">
+                ДАУНХИЛл
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
